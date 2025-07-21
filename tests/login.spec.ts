7c49d99c7491f35abec5dbe6a9240386a2bf8861
Michael Adam <mikeadamathome@gmail.com>
2025-07-21 14:56:50 +0200

    commit: second test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -10,3 +10,29 @@ await page.click('#login-button');
 
 await expect(page).toHaveURL(/inventory.html/);
 });
+
+test('Login with locked_out_user returns Logged out message', async({page}) => {
+
+    await page.goto('https://saucedemo.com/');
+    await page.fill('#user-name','locked_out_user');
+    await page.fill('#password','secret_sauce');
+    await page.click('#login-button');
+
+    const errorMessageLocator = page.locator('[data-test="error"]');
+    await expect(errorMessageLocator).toBeVisible();
+    await expect(errorMessageLocator).toContainText('Epic sadface: Sorry, this user has been locked out.');
+
+});
+
+test('Login with locked_out_user returns Logged out message', async({page}) => {
+
+    await page.goto('https://saucedemo.com/');
+    await page.fill('#user-name','locked_out_user');
+    await page.fill('#password','secret_sauce');
+    await page.click('#login-button');
+
+    const errorMessageLocator = page.locator('[data-test="error"]');
+    await expect(errorMessageLocator).toBeVisible();
+    await expect(errorMessageLocator).toContainText('Epic sadface: Sorry, this user has been locked out.');
+
+});
