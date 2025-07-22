490f8db1a0dd39dd606c48a5baa90f16d0ad8277
Michael Adam <mikeadamathome@gmail.com>
2025-07-22 16:34:15 +0200

    commit: second test updated with error button validation

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -16,23 +16,16 @@ test('Login with locked_out_user returns Logged out message', async({page}) => {
     await page.goto('https://saucedemo.com/');
     await page.fill('#user-name','locked_out_user');
     await page.fill('#password','secret_sauce');
-    await page.click('#login-button');
+    await page.click('#login-button'); 
 
+    //check the error message appears
     const errorMessageLocator = page.locator('[data-test="error"]');
     await expect(errorMessageLocator).toBeVisible();
     await expect(errorMessageLocator).toContainText('Epic sadface: Sorry, this user has been locked out.');
-
-});
-
-test('Login with locked_out_user returns Logged out message', async({page}) => {
-
-    await page.goto('https://saucedemo.com/');
-    await page.fill('#user-name','locked_out_user');
-    await page.fill('#password','secret_sauce');
-    await page.click('#login-button');
-
-    const errorMessageLocator = page.locator('[data-test="error"]');
-    await expect(errorMessageLocator).toBeVisible();
-    await expect(errorMessageLocator).toContainText('Epic sadface: Sorry, this user has been locked out.');
-
+    //check the error button exists
+    const errorMessageBUtton = page.locator('.error-button');
+    await expect(errorMessageBUtton).toBeVisible();
+    //check the button can be clicked and the error message gets hidden
+    await errorMessageBUtton.click();
+    await expect(errorMessageBUtton).toBeHidden();
 });
