5ace48bd9d679bd7141e16b5f30e086f8833c30d
Michael Adam <mikeadamathome@gmail.com>
2025-07-23 16:09:27 +0200

    commit: correct first test. Add performance test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -13,16 +13,17 @@ test('Login with standard_user succeeds', async ({page}) => {
     //check the first item is correct
     const firstValidItem = page.locator('.inventory_item').first();
     const FirstItemImage = firstValidItem.locator('.inventory_item_img img');
-    //const FirstItemTitle = firstValidItem.locator('.inventory-item-name ');
-    //const FirstItemDescription = firstValidItem.locator('.inventory-item-desc');
     const FirstItemImageSrc = await FirstItemImage.getAttribute('src');
+    const FirstItemTitle = page.locator('.inventory_item_name ').first();
+    const FirstItemDescription = page.locator('.inventory_item_desc').first();
+    const FirstItemPrice = page.locator('.inventory_item_price').first();
+
     
     expect(FirstItemImage).toBeVisible;
     expect(FirstItemImageSrc).toContain('sauce-backpack-1200x1500.0a0b85a3.jpg');
-    //console.log('ITEM TITLE:', await FirstItemTitle.textContent());
-    //await expect(FirstItemTitle).toHaveText('Sauce Labs Backpack');
-    //await expect(FirstItemDescription).toContainText('carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.');
-
+    await expect(FirstItemTitle).toHaveText('Sauce Labs Backpack');
+    await expect(FirstItemDescription).toContainText('carry.allTheThings() with the sleek, streamlined');
+    await expect(FirstItemPrice).toContainText('$29.99');
 });
 
 test('Login with locked_out_user returns Logged out message', async({page}) => {
@@ -60,5 +61,26 @@ test('Login with Problem User brower prompts user to change password', async({pa
 
     //console logging
     console.log('problem_user image src', imageSrc);
-    console.warn('Warning: Crhome displays message pop-up, warning the user to change their password.');
+    console.warn('Warning: Chrome displays message pop-up, warning the user to change their password.');
+});
+
+test('Login with performance_glitch_user succeeds, log to the console the abnormal time.', async ({page}) => {
+
+    await page.goto('https://saucedemo.com/');
+    await page.fill('#user-name','standard_user');
+    await page.fill('#password','secret_sauce');
+    await page.click('#login-button');
+
+    //assert the correct page loads
+    const startTime = Date.now();
+    await expect(page).toHaveURL(/inventory.html/, {timeout: 10000});
+    const loadTime = Date.now() - startTime;
+
+    //log to the console the load time
+    
+    if (loadTime > 2000)
+    {
+    console.log('Page load time: $(loadTime)ms')
+    console.warn('Warning: Page load time exceeded 2000ms')
+    }
 });
